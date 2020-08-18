a7903e57f5f3fae8f4b55451607b08656a27d45e
davidyang2149 <davidyang2149@gmail.com>
2020-08-18 21:46:40 +0900

    [Add]diabetes blog diabetes add input Add

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,11 @@
+import Router from "next/router";
+import "nprogress/nprogress.css";
+import Nprogress from "nprogress";
+import "antd/dist/antd.css";
+
+Router.events.on("routeChangeStart", () => Nprogress.start());
+Router.events.on("routeChangeComplete", () => Nprogress.done());
+
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,53 @@
+import React from "react";
 import Head from "next/head";
+import Router from "next/router";
+import Error from "next/error";
+import axios from "axios";
+import _ from "lodash";
+import produce from "immer";
+import moment from "moment";
+import { DatePicker } from "antd";
 
-export default function Home() {
+export default function Home(props) {
+  if (props.error) {
+    return <Error statusCode={500} title={props.error.message} />;
+  }
+  //if (props.data.faultInfo) {
+  //  return <Error statusCode={500} title={props.data.faultInfo.message} />;
+  //}
+
+  const targetDt = moment().subtract(1, "day").format("YYYYMMDD");
+  //props.targetDt = targetDt;
+
   return (
     <div>
       <Head>
         <title>Diabetes Blog</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <h1 className="text-4xl font-bold">당뇨 체크</h1>
+      <div>
+        <DatePicker
+          defaultValue={moment(targetDt, "YYYYMMDD")}
+          //defaultValue={moment(props.targetDt, "YYYYMMDD")}
+          dateFormat={"YYYYMMDD"}
+          onChange={(date) =>
+            Router.push("/?targetDt=" + date.format("YYYYMMDD"))
+          }
+        />
+        <input
+          type="text"
+          className="border p-1"
+          //value={text}
+          //onChange={(event) => setText(event.target.value)}
+        />
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-4 rounded"
+          //onClick={addItem}
+        >
+          추가
+        </button>
+      </div>
     </div>
   );
 }
